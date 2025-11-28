610d1467118aa03b853ac0a5e5bec540f8645c72
Baasankhuu Purev <purevbaasankhuu@gmail.com>
2025-11-28 19:17:58 +0700

    Update API version to v2.0.0Hd

--- routes/success.js
+++ routes/success.js
@@ -3,7 +3,7 @@ const router = express.Router();
 router.route("/").get((req, res) => {
   res.status(200).json({
     message: {
-      version: "v1.0.0",
+      version: "v2.0.0",
       message: "RATING New API is here",
       date: "2025-10-04"
     },
