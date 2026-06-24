1c3ad55d37023a4088368b20dddea0ecf6ae3d1d
mind-loop <purevbaasankhuu@gmail.com>
2026-06-25 01:31:55 +0800

    fxd

--- routes/success.js
+++ routes/success.js
@@ -3,9 +3,9 @@ const router = express.Router();
 router.route("/").get((req, res) => {
   res.status(200).json({
     message: {
-      version: "v2.0.12",
+      version: "v2.0.13",
       message: "AI Analize sent successfully fixed!",
-      date: "2026-05-07"
+      date: "2026-06-25"
     },
     success: true,
   });
